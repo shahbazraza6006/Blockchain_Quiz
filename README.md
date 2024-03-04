2e6540c53bf6e33fe6e58f4a1720352ee49dbed3
shahbazraza6006 <i201761@nu.edu.pk>
2024-03-04 22:19:18 +0500

    First Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Blockchain_Quiz
 This is a quiz developed in Go Lang 
+
+To create branch i run the following command
+
+**git branch dev**
+
+Then i checkout to dev branch using the following command
+
+**git checkout dev**
+
+
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func DisplayAllBlocks() {
+	// Implementation for DisplayAllBlocks
+	fmt.Println("Displaying all blocks...")
+}
+
+func NewBlock() {
+	// Implementation for NewBlock
+	fmt.Println("Creating a new block...")
+}
+
+func ModifyBlock() {
+	// Implementation for ModifyBlock
+	fmt.Println("Modifying a block...")
+}
+
+func main() {
+	// Call the functions
+	DisplayAllBlocks()
+	NewBlock()
+	ModifyBlock()
+
+	fmt.Println("Program executed successfully.")
+}
